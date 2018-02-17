f8ed6b17d5a3917eb320b7723db3e6722f86e052
Andy CA6JAU <juribeparada@gmail.com>
2018-02-17 00:12:49 -0300

    Update version

--- version.h
+++ version.h
@@ -23,9 +23,9 @@
 #include "ADF7021.h"
 
 #define VER_MAJOR       "1"
-#define VER_MINOR       "2"
-#define VER_REV         "1"
-#define VERSION_DATE    "20180214"
+#define VER_MINOR       "3"
+#define VER_REV         "0"
+#define VERSION_DATE    "20180217"
 
 #if defined(ZUMSPOT_ADF7021)
 #define BOARD_INFO      "ZUMspot"
