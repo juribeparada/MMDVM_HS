4c9c67286ca1488aefa8cd404fd43d27e9342319
Tom <BG4TGO@users.noreply.github.com>
2018-04-06 22:14:42 +0800

    Update install_fw_nanodv.sh

--- scripts/install_fw_nanodv.sh
+++ scripts/install_fw_nanodv.sh
@@ -73,5 +73,5 @@ fi
 sudo killall MMDVMHost >/dev/null 2>&1
 
 # Upload the firmware
-eval sudo $STM32FLASH -v -w nano_hotspot_fw.bin -g 0x0 -R -i 10,-14,14:-10,14 /dev/ttyAMA0
+eval sudo $STM32FLASH -v -w nano_dv_fw.bin -g 0x0 -R -i 67,-66,66:-67,66 /dev/ttyAMA0
 
