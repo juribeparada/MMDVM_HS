bd6217a4a6d15af0771f9f256299bcb3dec59d3b
Andy CA6JAU <juribeparada@gmail.com>
2018-05-21 15:25:43 -0400

    Update version

--- version.h
+++ version.h
@@ -24,7 +24,7 @@
 
 #define VER_MAJOR       "1"
 #define VER_MINOR       "3"
-#define VER_REV         "5"
+#define VER_REV         "6"
 #define VERSION_DATE    "20180521"
 
 #if defined(ZUMSPOT_ADF7021)
