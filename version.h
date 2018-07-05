dedb41954fb2fc0edee739bcd89e063d6015b9f4
Andy CA6JAU <juribeparada@gmail.com>
2018-07-05 00:11:33 -0400

    Revert "Optimizing bit memory ring buffer"
    This reverts commit 2e2d2b30046f8146a25d93d3ce1e34f12a000f41.

--- version.h
+++ version.h
@@ -24,7 +24,7 @@
 
 #define VER_MAJOR       "1"
 #define VER_MINOR       "4"
-#define VER_REV         "1"
+#define VER_REV         "0"
 #define VERSION_DATE    "20180705"
 
 #if defined(ZUMSPOT_ADF7021)
