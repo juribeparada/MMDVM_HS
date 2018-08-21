b14548e9db68d522787cc16f5cf4e5d26ecf7b89
Andy CA6JAU <juribeparada@gmail.com>
2018-08-21 20:20:41 -0300

    Preparing next release

--- version.h
+++ version.h
@@ -24,8 +24,8 @@
 
 #define VER_MAJOR       "1"
 #define VER_MINOR       "4"
-#define VER_REV         "6"
-#define VERSION_DATE    "20180720"
+#define VER_REV         "7"
+#define VERSION_DATE    "20180821"
 
 #if defined(ZUMSPOT_ADF7021)
 #define BOARD_INFO      "ZUMspot"
