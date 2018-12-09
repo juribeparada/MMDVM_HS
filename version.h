a9fed35d1c5aadd7f6856df06ad293980026916a
Andy CA6JAU <juribeparada@gmail.com>
2018-12-09 18:59:56 -0300

    Update version

--- version.h
+++ version.h
@@ -24,8 +24,8 @@
 
 #define VER_MAJOR       "1"
 #define VER_MINOR       "4"
-#define VER_REV         "13"
-#define VERSION_DATE    "20181204"
+#define VER_REV         "14"
+#define VERSION_DATE    "20181209"
 
 #if defined(ZUMSPOT_ADF7021)
 #define BOARD_INFO      "ZUMspot"
